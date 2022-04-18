d986a9cec950f24f611b62586b93f1786a72b12e
Syaiful Amri <syaiful_amri@windowslive.com>
2022-04-18 13:37:41 +0700

    Add files via upload

---
+++ application/views/templates/Member_Footer.php
@@ -0,0 +1,21 @@
+<footer class="footer">
+  <div class="container">
+    <div class="copyright">
+      <p>© 2022 - Aplikasi Diteksi Kerusakan Komputer | Dibuat oleh<a href="https://amrysyaiful.blogspot.com/" target="_blank" rel="noopener noreferrer"> U.R.M</a></p>
+    </div>
+  </div>
+</footer>
+</div>
+
+
+
+</body>
+
+<script src="<?= base_url('assets2'); ?>/js/jquery-3.1.1.min.js" type="text/javascript"></script>
+<script src="<?= base_url('assets2'); ?>/js/jquery.easing.1.3.js" type="text/javascript"></script>
+<script src="<?= base_url('assets2'); ?>/js/bootstrap.js" type="text/javascript"></script>
+<script src="<?= base_url('assets2'); ?>/js/awesome-landing-page.js" type="text/javascript"></script>
+<script src="<?= base_url('assets2'); ?>/js/script.js" type="text/javascript">
+</script>
+
+</html>
